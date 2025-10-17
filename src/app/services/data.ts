2fda771fbfc615c3f691b263cae2c0d5329c33c3
saizge21-a11y <saizge21@gmail.com>
2025-10-16 18:00:12 -0600

    route

--- src/app/services/data.ts
+++ src/app/services/data.ts
@@ -23,15 +23,15 @@ export class DataServices {
 
   constructor(private http: HttpClient) {
     this.myAppUrl = environment.apiUrl;
-    this.myApiUrl = '/api/pais/';
-    this.myApiUrlDep = '/api/departamento/';
-    this.myApiUrlMun = '/api/municipio/';
-    this.myApiUrlEm = '/api/empresa/';
-    this.myApiUrlTr = '/api/trabajador/';
-    this.myApiUrlAsig = '/api/empresa-trabajador/'; 
+    this.myApiUrl = '/pais/';
+    this.myApiUrlDep = '/departamento/';
+    this.myApiUrlMun = '/municipio/';
+    this.myApiUrlEm = '/empresa/';
+    this.myApiUrlTr = '/trabajador/';
+    this.myApiUrlAsig = '/empresa-trabajador/'; 
   }
 
-  // --- País ---W
+  // --- País ---
   getListPaises(): Observable<{ listPais: Pais[] }> {
     return this.http.get<{ listPais: Pais[] }>(this.myAppUrl + this.myApiUrl);
   }
