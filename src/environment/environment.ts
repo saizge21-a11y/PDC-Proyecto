32a6bfb7175ceb6ce6aab1672bca8092701564a8
saizge21-a11y <saizge21@gmail.com>
2025-10-16 18:09:57 -0600

    fix api urls

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://pdc-backend-production.up.railway.app/api'
+  apiUrl: 'https://pdc-backend-production.up.railway.app/api/'
 };
 
